e5e0251e5e055dacd4585d8f27e7fd202fdef7fa
Sameer Brenn <sameer@creativedestruction.com>
2024-05-08 12:11:00 -0600

    basic structure

--- src/operations.ts
+++ src/operations.ts
@@ -1,21 +1,48 @@
-import { TransactionContext, Transaction, GetApi, ArgSource, ArgSources } from '@dbos-inc/dbos-sdk';
+import { TransactionContext, CommunicatorContext, Transaction, GetApi, ArgSource, ArgSources, PostApi, Workflow, Communicator, WorkflowContext } from '@dbos-inc/dbos-sdk';
 import { Knex } from 'knex';
 
 // The schema of the database table used in this example.
-export interface dbos_hello {
-  name: string;
-  greet_count: number;
+
+enum Side {
+  Buy, Sell
+}
+export interface Order {
+  id: number;
+  side: Side;
+  price: number;
+  size: number;
+  trader: number;
+}
+
+export interface Fill {
+
 }
 
 export class Hello {
 
-  @GetApi('/greeting/:user') // Serve this function from HTTP GET requests to the /greeting endpoint with 'user' as a path parameter
-  @Transaction()  // Run this function as a database transaction
-  static async helloTransaction(ctxt: TransactionContext<Knex>, @ArgSource(ArgSources.URL) user: string) {
+
+  @Transaction()
+  static async insertOrder(ctxt: TransactionContext<Knex>, order: Order) {
+
+  }
+
+  @Transaction()
+  static async findMatches(ctxt: TransactionContext<Knex>): Promise<Fill[]> {
+    return [];
+  }
+
+  @Communicator()
+  static async sendFills(ctxt: CommunicatorContext, fills: Fill[]) {
+
+  }
+
+  @PostApi('/order') // Serve this function from HTTP GET requests to the /greeting endpoint with 'user' as a path parameter
+  @Workflow()  // Run this function as a database transaction
+  static async helloTransaction(ctxt: WorkflowContext, @ArgSource(ArgSources.BODY) order: Order) {
     // Retrieve and increment the number of times this user has been greeted.
-    const query = "INSERT INTO dbos_hello (name, greet_count) VALUES (?, 1) ON CONFLICT (name) DO UPDATE SET greet_count = dbos_hello.greet_count + 1 RETURNING greet_count;";
-    const { rows } = await ctxt.client.raw(query, [user]) as { rows: dbos_hello[] };
-    const greet_count = rows[0].greet_count;
-    return `Hello, ${user}! You have been greeted ${greet_count} times.\n`;
+
+    await ctxt.invoke(Hello).insertOrder(order);
+    var fills = await ctxt.invoke(Hello).findMatches();
+    ctxt.invoke(Hello).sendFills(fills);
   }
 }
